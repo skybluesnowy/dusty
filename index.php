c64f429129c80b39e05939a81bf7f69e4e0aab76
Snowy Snowman <spamjavelin@moodychops.com>
2014-05-16 19:45:14 +0100

    initial push

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js ie lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js ie lt-ie10 lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js ie lt-ie10 lt-ie9" lang="en"><![endif]-->
+<!--[if IE 9]><html class="no-js ie lt-ie10" lang="en"><![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title></title>
+
+<meta name="Description" content="">
+<meta name="Keywords" content="">
+
+<link href="/static/css/global.css" rel="stylesheet" type="text/css">
+<!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+<p>hello</p>
+<p class="logo">hp</p>
+</body>
+</html>
